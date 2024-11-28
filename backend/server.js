191580818af648360858365ba501d80ad0f20c51
WickeyR <149520847+WickeyR@users.noreply.github.com>
2024-11-27 21:56:01 -0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -940,7 +940,7 @@ app.get('/dashboard', (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}`);
 });
